825b04dd70faed3935f9fd1611d296b8dd130a0a
vinybergamo <vinybergamo@gmail.com>
2023-06-01 23:19:41 -0300

    feat: create register modules and commands

--- src/struct/client.ts
+++ src/struct/client.ts
@@ -1,6 +1,8 @@
 import {
+  ApplicationCommandDataResolvable,
   BitFieldResolvable,
   ClientEvents,
+  Collection,
   Client as DiscordClient,
   GatewayIntentsString,
   IntentsBitField,
@@ -10,12 +12,25 @@ import { config } from "dotenv";
 import * as path from "path";
 import * as fs from "fs";
 import { EventType } from "./types/Event";
+import {
+  CommandType,
+  ComponentsButton,
+  ComponentsModal,
+  ComponentsRoleSelect,
+  ComponentsSelect,
+} from "./types/Commands";
 config();
 
 const fileFilter = (filename: string) =>
   filename.endsWith(".ts") || filename.endsWith(".js");
 
 export class Client extends DiscordClient {
+  public commands: Collection<String, CommandType> = new Collection();
+  public buttons: ComponentsButton = new Collection();
+  public selects: ComponentsSelect = new Collection();
+  public rolesSelectMenu: ComponentsRoleSelect = new Collection();
+  public modals: ComponentsModal = new Collection();
+
   constructor() {
     super({
       intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<
@@ -36,9 +51,57 @@ export class Client extends DiscordClient {
   }
 
   public async start() {
+    this.registerModules();
     this.registerEvents();
     this.login(process.env.TOKEN);
     this.once("ready", this.whenReady);
+  }
+
+  private registerCommands(commands: Array<ApplicationCommandDataResolvable>) {
+    this.application?.commands
+      .set(commands)
+      .catch((e) =>
+        console.log(
+          `An error occurred while trying to set the Slash Commands (/): \n${e}`
+            .red
+        )
+      );
+  }
+
+  private registerModules() {
+    const slashCommands: Array<ApplicationCommandDataResolvable> = new Array();
+
+    const commandsPath = path.join(__dirname, "..", "commands");
+
+    fs.readdirSync(commandsPath).forEach((dir) => {
+      fs.readdirSync(commandsPath + `/${dir}/`)
+        .filter(fileFilter)
+        .forEach(async (fileName) => {
+          const command: CommandType = (
+            await import(`../commands/${dir}/${fileName}`)
+          )?.default;
+
+          const { name, buttons, selects, modals, rolesSelect } = command;
+
+          if (name) {
+            this.commands.set(name, command);
+            slashCommands.push(command);
+            if (buttons)
+              buttons.forEach((run, key) => this.buttons.set(key, run));
+            if (selects)
+              selects.forEach((run, key) => this.selects.set(key, run));
+            if (modals) modals.forEach((run, key) => this.modals.set(key, run));
+            if (rolesSelect)
+              rolesSelect.forEach((run, key) =>
+                this.rolesSelectMenu.set(key, run)
+              );
+          }
+        });
+    });
+
+    this.on("ready", () => {
+      this.registerCommands(slashCommands);
+    });
   }
 
   private registerEvents() {
